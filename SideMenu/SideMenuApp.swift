625bf3ab3b8c5e861191ec50eb690cfa706b721a
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-18 21:19:48 +0200

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Y K on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
